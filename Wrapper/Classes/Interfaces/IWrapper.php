318f745be0bc8472416430922f264e2ca95b77eb
Ilja Kovalenko <dalek@mail.ua>
2019-09-20 14:47:45 +0300

    reformat code

--- Wrapper/Classes/Interfaces/IWrapper.php
+++ Wrapper/Classes/Interfaces/IWrapper.php
@@ -4,9 +4,14 @@
 interface IWrapper
 {
     public function getById($id);
+
     public function getPage($page);
+
     public function query(array $data);
+
     public function getItemFromJson();
+
     public function getMetaFromJson(): Meta;
+
     public function getLinksFromJson(): Links;
 }
